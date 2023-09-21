61b1daa65a913f00017a704d12aaafd5ffb9af47
Jay Lo <jaylo@kkstream.com>
2023-09-21 17:32:35 +0800

    Add RTMP sample called HaishinKit.swift-main

---
+++ Samples/HaishinKit.swift-main/Examples/iOS/Preference.swift
@@ -0,0 +1,6 @@
+struct Preference {
+    static var defaultInstance = Preference()
+
+    var uri: String? = "rtmp://87d125a92b94d1a84731fc70dda1e4a5-main.jcl-qa-coco-d3.kkstream.tech:1935/live/stream"
+    var streamName: String? = "stream"
+}
---
+++ Samples/HaishinKit.swift-main/Tests/Codec/AudioCodecTests.swift
@@ -0,0 +1,97 @@
+import Foundation
+import XCTest
+import AVFoundation
+
+@testable import HaishinKit
+
+final class AudioCodecTests: XCTestCase {
+    func testEncoderCMSampleBuffer44100_1024() {
+        let encoder = AudioCodec()
+        encoder.startRunning()
+        for _ in 0..<10 {
+            if let sampleBuffer = CMAudioSampleBufferFactory.makeSinWave(44100, numSamples: 1024) {
+                encoder.appendSampleBuffer(sampleBuffer)
+            }
+        }
+    }
+
+    func testEncoderCMSampleBuffer48000_1024() {
+        let encoder = AudioCodec()
+        encoder.startRunning()
+        for _ in 0..<10 {
+            if let sampleBuffer = CMAudioSampleBufferFactory.makeSinWave(48000.0, numSamples: 1024) {
+                encoder.appendSampleBuffer(sampleBuffer)
+            }
+        }
+    }
+
+    func testEncoderCMSampleBuffer24000_1024() {
+        let encoder = AudioCodec()
+        encoder.startRunning()
+        for _ in 0..<10 {
+            if let sampleBuffer = CMAudioSampleBufferFactory.makeSinWave(24000.0, numSamples: 1024) {
+                encoder.appendSampleBuffer(sampleBuffer)
+            }
+        }
+    }
+
+    func testEncoderCMSampleBuffer16000_1024() {
+        let encoder = AudioCodec()
+        encoder.startRunning()
+        for _ in 0..<10 {
+            if let sampleBuffer = CMAudioSampleBufferFactory.makeSinWave(16000.0, numSamples: 1024) {
+                encoder.appendSampleBuffer(sampleBuffer)
+            }
+        }
+    }
+
+    func testEncoderCMSampleBuffer8000_256() {
+        let encoder = AudioCodec()
+        encoder.startRunning()
+        for _ in 0..<10 {
+            if let sampleBuffer = CMAudioSampleBufferFactory.makeSinWave(8000.0, numSamples: 256) {
+                encoder.appendSampleBuffer(sampleBuffer)
+            }
+        }
+    }
+
+    func testEncoderCMSampleBuffer8000_960() {
+        let encoder = AudioCodec()
+        encoder.startRunning()
+        for _ in 0..<10 {
+            if let sampleBuffer = CMAudioSampleBufferFactory.makeSinWave(8000.0, numSamples: 960) {
+                encoder.appendSampleBuffer(sampleBuffer)
+            }
+        }
+    }
+
+    func testEncoderCMSampleBuffer8000_1224() {
+        let encoder = AudioCodec()
+        encoder.startRunning()
+        for _ in 0..<10 {
+            if let sampleBuffer = CMAudioSampleBufferFactory.makeSinWave(44100.0, numSamples: 1224) {
+                encoder.appendSampleBuffer(sampleBuffer)
+            }
+        }
+    }
+
+    func testEncoderCMSampleBuffer8000_numSamples() {
+        let numSamples: [Int] = [1024, 1024, 1028, 1024, 1028, 1028, 962, 962, 960, 2237, 2236]
+        let encoder = AudioCodec()
+        encoder.startRunning()
+        for numSample in numSamples {
+            if let sampleBuffer = CMAudioSampleBufferFactory.makeSinWave(44100.0, numSamples: numSample) {
+                encoder.appendSampleBuffer(sampleBuffer)
+            }
+        }
+    }
+
+    func test3Channel_withoutCrash() {
+        let encoder = AudioCodec()
+        encoder.startRunning()
+        if let sampleBuffer = CMAudioSampleBufferFactory.makeSilence(44100, numSamples: 256, channels: 3) {
+            encoder.appendSampleBuffer(sampleBuffer)
+        }
+    }
+}
+
